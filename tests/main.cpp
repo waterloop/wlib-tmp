2890ab01e6dd7aa7224696fe9040bb6fff59348a
Jeff Niu <me@jeffniu.com>
2018-09-29 12:53:14 -0400

    update to wio 0.4.0 (#2)

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,33 @@
 #include <wlib/utility>
+#include <cassert>
 
-int main(int argc, char *argv[]) {}
+class Mover {
+public:
+    Mover() : m_ptr(new int) {}
+
+    Mover(Mover &&m) : m_ptr(m.m_ptr) {
+        m.m_ptr = nullptr;
+    }
+
+    ~Mover() {
+        if (nullptr != m_ptr) {
+            delete m_ptr;
+        }
+    }
+
+    int *get() {
+        return m_ptr;
+    }
+private:
+    int *m_ptr;
+};
+
+int main(int argc, char *argv[]) {
+    // wlp::move
+    Mover m1;
+    assert(nullptr != m1.get());
+    *m1.get() = 10;
+    Mover m2(wlp::move(m1));
+    assert(nullptr == m1.get());
+    assert(10 == *m2.get());
+}
